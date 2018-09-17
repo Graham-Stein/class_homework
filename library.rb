8c35b9d56e5777d88f08cd8a619fbe878cf42cd0
Graham-Stein <graham@guavoaviation.co.uk>
2018-09-17 21:28:42 +0100

    Almost completes extension

--- library.rb
+++ library.rb
@@ -1,8 +1,32 @@
 class Library
 
-  def intialize()
+  attr_accessor :book
+
+  def initialize(book)
+    @book = book
+
 
   end
 
+  def get_book_name
+    @book[0][:title]
+  end
+
+  def get_student_name
+    @book[0][:rental_details][:student_name]
+  end
+
+  def get_due_date
+    @book[0][:rental_details][:date]
+  end
+
+  def get_book_details(sought_book)
+    return @book if @book[0][:title] == sought_book
+  end
+
+  def get_book_rental_details(sought_book)
+    return @book[0][:rental_details] if @book[0][:title] == sought_book
+
+  end
 
 end
--- specs/library_spec.rb
+++ specs/library_spec.rb
@@ -5,12 +5,47 @@ require_relative('../library.rb')
 class TestLibrary < MiniTest::Test
 
   def test_array_contains_books
-    edinburgh_library = Library.new(["Practical Object-Oriented Design in Ruby", "Larry Love", "20/08/17")
-    assert_equal("Practical Object-Oriented Design in Ruby", edinburgh_library[0][:title])
-    # assert_equal("Larry Love",edinburgh_library[0][:rental_details][:student_name])
-    # assert_equal("20/08/17",edinburgh_library[0][:rental_details][:date])
+    edinburgh_library = Library.new([{
+    title: "Practical Object-Oriented Design in Ruby",
+    rental_details: {
+     student_name: "Larry Love",
+     date: "20/08/17"
+    }
+  }])
+    assert_equal("Practical Object-Oriented Design in Ruby", edinburgh_library.get_book_name)
+    assert_equal("Larry Love",edinburgh_library.get_student_name)
+    assert_equal("20/08/17",edinburgh_library.get_due_date)
   end
 
+  def test_get_book_details
+    edinburgh_library = Library.new([{
+    title: "Practical Object-Oriented Design in Ruby",
+    rental_details: {
+     student_name: "Larry Love",
+     date: "20/08/17"
+    }
+  }])
+    assert_equal([{
+    title: "Practical Object-Oriented Design in Ruby",
+    rental_details: {
+     student_name: "Larry Love",
+     date: "20/08/17"
+    }
+  }], edinburgh_library.get_book_details("Practical Object-Oriented Design in Ruby"))
+  end
 
+  def test_get_book_rental_details
+    edinburgh_library = Library.new([{
+    title: "Practical Object-Oriented Design in Ruby",
+    rental_details: {
+     student_name: "Larry Love",
+     date: "20/08/17"
+    }
+  }])
+    assert_equal({
+     student_name: "Larry Love",
+     date: "20/08/17"
+    }, edinburgh_library.get_book_rental_details("Practical Object-Oriented Design in Ruby"))
+  end
 
 end
